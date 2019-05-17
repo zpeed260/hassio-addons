266bf7efc44f540f5481f58907801496a05c1769
zpeed260 <50737732+zpeed260@users.noreply.github.com>
2019-05-17 20:25:38 +1000

    Update run.sh

--- letsdnsocloud/run.sh
+++ letsdnsocloud/run.sh
@@ -2,14 +2,9 @@
 . functions.sh
 set -e
 
-CERT_DIR=/data/letsencrypt
+
 WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
-
-# Let's encrypt Deets
-LE_TERMS=$(jq --raw-output '.lets_encrypt.accept_terms' $CONFIG_PATH)
-LE_DOMAINS=$(jq --raw-output '.domains[]' $CONFIG_PATH)
-LE_UPDATE="0"
 
 #CloudFlare Deets
 export CF_APIKEY=$(jq --raw-output '.cfapikey' $CONFIG_PATH)
@@ -35,30 +30,6 @@ if [ -z "$AID" ]
     updateip $IP
 fi
 
-# Register/generate certificate if terms accepted
-
-function le_renew() {
-    local domain_args=()
-    # Prepare domain for Let's Encrypt
-    for domain in $LE_DOMAINS; do
-        domain_args+=("--domain" "$domain")
-    done
-    dehydrated --cron --hook ./hooks.sh --challenge dns-01 "${domain_args[@]}" --out "$CERT_DIR" --config "$WORK_DIR/config" || true
-    LE_UPDATE="$(date +%s)"
-}
-
-if [ "$LE_TERMS" == "true" ]; then
-    # Init folder structs
-    mkdir -p "$CERT_DIR"
-    mkdir -p "$WORK_DIR"
-
-    # Generate new certs
-    if [ ! -d "$CERT_DIR/live" ]; then
-        # Create empty dehydrated config file so that this dir will be used for storage
-        touch "$WORK_DIR/config"
-        dehydrated --register --accept-terms --config "$WORK_DIR/config"
-    fi
-fi
 
 # Loop: Watch for new IP and update. Renew Cert after 30 days
 while true; do
@@ -70,8 +41,5 @@ while true; do
     fi
 
     now="$(date +%s)"
-    if [ "$LE_TERMS" == "true" ] && [ $((now - LE_UPDATE)) -ge 43200 ]; then
-        le_renew
-    fi
     sleep "$WAIT_TIME"
 done
